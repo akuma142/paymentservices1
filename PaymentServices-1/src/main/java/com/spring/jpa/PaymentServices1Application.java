b6689f5ba1cc9b9542ed5678b97573285b66fcb9
Attiq <ashu.pat793@gmail.com>
2021-06-07 08:03:00 -0700

    first commit

---
+++ PaymentServices-1/src/main/java/com/spring/jpa/PaymentServices1Application.java
@@ -0,0 +1,13 @@
+package com.spring.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServices1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServices1Application.class, args);
+	}
+
+}
